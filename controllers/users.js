cbb22ca4eca584b9b678f0e00f3c33ac6246acc6
Neilketchum <daipayanh@gmail.com>
2020-11-22 18:17:34 +0530

    Add Sign up Page and Form

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,15 @@
 module.exports = function(_){
     return {
         SetRouting : function(router){
-            router.get('/',this.indexPage)
+            router.get('/',this.indexPage);
+            router.get('/signup',this.signupPage);
+         
         },
         indexPage:function(req,res){
             return res.render('index',{test:"This is Test"})
+        },
+        signupPage:function(req,res){
+            return res.render('signup')
         }
-    }   
+    }
 }
--- server.js
+++ server.js
@@ -1,32 +1,62 @@
 const express = require('express')
 const bodyParser = require("body-parser")
-const ejs = require('ejs')
 const http = require('http')
-const container  = require('./container');
+const cookieParser = require('cookie-parser')
+const validator = require('express-validator');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
+const flash = require("connect-flash")
+const passport = require('passport')
 
-container.resolve(function(users){
+
+const container = require('./container');
+
+container.resolve(function (users) {
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb+srv://neil:123@cluster0.ymdvf.mongodb.net/<dbname>?retryWrites=true&w=majority",
+        {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then((req, res) => {
+            if (req) {
+                console.log("Connected")
+            } else {
+                console.log("rejected")
+            }
+        })
     const app = SetupExpress();
 
-
-    function SetupExpress(){
+    function SetupExpress() {
         const app = express();
         const server = http.createServer(app);
-        server.listen(8080,function(){
+        server.listen(8080, function () {
             console.log(`server running at port 8080`)
         })
         configureExpress(app);
-          // Set up Router
+        // Set up Router
         const router = require('express-promise-router')();
         users.SetRouting(router);
-        
-    
+
+
         app.use(router);
     }
-    function configureExpress(app){
+    function configureExpress(app) {
         app.use(express.static('public'));
-        app.set('view engine','ejs');
+        app.set('view engine', "ejs");
         app.set(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended:true}));
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(validator());
+        app.use(session({
+            secret: "janclncaklnkcnalcnlkcnxcnca",
+            resave: "true",
+            saveUninitialized: true,
+            store: new MongoStore({ mongooseConnection: mongoose.connection })
+        }))
+        app.use(flash())
+        app.use(passport.initialize());
+        app.use(passport.session());
 
     }
 })
